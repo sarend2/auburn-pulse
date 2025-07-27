7c4cb0cf0da51c32bf90af2ab9d5cab2219282ba
sarend2 <callahamstephen@gmail.com>
2025-07-27 10:27:52 -0400

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -5,36 +5,54 @@ export default function Sitemap() {
 }
 
 export async function getServerSideProps({ res }) {
+  // Get current date in YYYY-MM-DD format
+  const today = new Date().toISOString().split('T')[0];
+  
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://theauburnpulse.com</loc>
-    <lastmod>2025-07-24</lastmod>
+    <lastmod>${today}</lastmod>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://theauburnpulse.com/eat</loc>
-    <lastmod>2025-07-24</lastmod>
+    <lastmod>${today}</lastmod>
     <priority>0.9</priority>
   </url>
   <url>
     <loc>https://theauburnpulse.com/explore</loc>
-    <lastmod>2025-07-24</lastmod>
+    <lastmod>${today}</lastmod>
     <priority>0.9</priority>
   </url>
   <url>
     <loc>https://theauburnpulse.com/fun</loc>
-    <lastmod>2025-07-24</lastmod>
+    <lastmod>${today}</lastmod>
     <priority>0.9</priority>
   </url>
   <url>
     <loc>https://theauburnpulse.com/featured</loc>
-    <lastmod>2025-07-24</lastmod>
+    <lastmod>${today}</lastmod>
     <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://theauburnpulse.com/legal</loc>
+    <lastmod>${today}</lastmod>
+    <priority>0.3</priority>
+  </url>
+  <url>
+    <loc>https://theauburnpulse.com/privacy</loc>
+    <lastmod>${today}</lastmod>
+    <priority>0.3</priority>
+  </url>
+  <url>
+    <loc>https://theauburnpulse.com/cookies</loc>
+    <lastmod>${today}</lastmod>
+    <priority>0.3</priority>
   </url>
 </urlset>`;
 
-  res.setHeader('Content-Type', 'text/xml');
+  res.setHeader('Content-Type', 'application/xml');
   res.write(sitemap);
   res.end();
 
